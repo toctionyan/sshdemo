a22047cdac12ba2dda640c85a6c9e903eb238e4e
toctionyan <1104730926@qq.com>
2024-07-19 18:12:35 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client } = require('ssh2');
 const conn = new Client();
 conn.on('ready', () => {
     console.log('Client :: ready');
-    conn.exec('ls', (err, stream) => {
+    conn.exec('cd ~/domains/$USER.serv00.net/vless && ./check_vless.sh', (err, stream) => {
         if (err) throw err;
         stream.on('close', (code, signal) => {
             console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
